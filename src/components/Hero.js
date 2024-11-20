7f9600ef1d2a1c1e63e924a5086035beda337bb5
Mohammed Thanis <thanis7168t@gmail.com>
2024-11-20 18:41:23 +0530

    profile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="font-poppins" id="Hero">
       <div className="max-w-7xl pl-5 ">
         <div className="flex gap-20 flex-col md:flex-row items-center  lg:h-[90vh] justify-between">
-          <div className="w-full md:w-1/2 mt-10 md:mt-0 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0">
+          <div className="w-full md:w-1/2 mt-10 md:mt-0 mb-0 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0">
             <h1 className="lg:text-6xl  text-4xl font-bold leading-snug">
               Hi There, <br /> I'm
               <span className="text-red-500"> Thanis</span>
@@ -25,7 +25,7 @@ const Hero = () => {
               Download CV
             </a>
           </div>
-          <div className="md:w-1/2 relative flex justify-center items-end bg-black h-[60vh] lg:h-[90vh] ">
+          <div className="md:w-1/2 relative flex justify-center items-end bg-black h-[40vh] lg:h-[90vh] md:h-[75vh] ">
             <img src={hero} alt="" className="lg:h-[100vh]  h-96" />
           </div>
         </div>
