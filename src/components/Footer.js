cd601f4369ea133d903c1fa3d40e0000255b0f35
Mohammed Thanis <thanis7168t@gmail.com>
2024-11-20 21:59:26 +0530

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const currentYear = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="bg-gray-100 py-4 relative">
-      <div className="container mx-auto px-4 text-center">
+      <div className="container mx-auto px-4 text-center ">
         <p>© {currentYear} Mohammed Thanis. All rights reserved.</p>
       </div>
     </footer>
