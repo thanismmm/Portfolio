9f24c9dff68f5c0421d2c72c06fe95dc68687430
Mohammed Thanis <thanis7168t@gmail.com>
2024-11-20 19:27:48 +0530

    autoyear

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className="bg-gray-100 py-4 relative">
       <div className="container mx-auto px-4 text-center">
-        <p>© 2024 Mohammed Thanis. All rights reserved.</p>
+        <p>© {currentYear} Mohammed Thanis. All rights reserved.</p>
       </div>
     </footer>
   );
